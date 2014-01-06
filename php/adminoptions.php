d76b2a953d6f67610f7a2bf69d98f63b0b5679d3
netguysteve <netguysteve@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-06 13:12:07 +0000

    Release Version 1.2.1 - Compatibility upgrade, reorganized options page, minor bug fix with status messages
    git-svn-id: https://plugins.svn.wordpress.org/ngs-sam-integrator/tags/1.2.1@833840 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ php/adminoptions.php
@@ -0,0 +1,100 @@
+<?php 
+/**
+ * HTML for Admin Options Panel
+ * 
+ * @package NGS SAM Integrator
+ * @since 0.1.0
+ */
+?>
+<div class=wrap>
+<form method="post" action="<?php echo $_SERVER["REQUEST_URI"]; ?>">
+	<img src="<?php echo $ngs_sam_logo; ?>" />
+	<h2>Net Guy Steve's SAM Integrator</h2>
+	
+	<?php
+		$active_tab = isset( $_GET[ 'tab' ] ) ? $_GET[ 'tab' ] : 'connections';
+	?>
+	
+	<h2 class="nav-tab-wrapper">
+		<a href="?page=ngs-sam-integrator&tab=connections" class="nav-tab <?php echo $active_tab == 'connections' ? 'nav-tab-active' : ''; ?>">Connections</a>
+		<a href="?page=ngs-sam-integrator&tab=pages" class="nav-tab <?php echo $active_tab == 'pages' ? 'nav-tab-active' : ''; ?>">Pages</a>
+		<a href="?page=ngs-sam-integrator&tab=status_messages" class="nav-tab <?php echo $active_tab == 'status_messages' ? 'nav-tab-active' : ''; ?>">Status Messages</a>
+	</h2>
+<?php if ('connections' == $active_tab) { ?>
+	<h2>SAM Broadcaster Software Settings</h2>
+	<h4>These settings to where the SAM Broadcaster software is running.</h4>
+	<h3>SAM Broadcaster Host IP Address</h3>
+<input type="text" name="ngs_sam_host" value="<?php echo $ngs_options['samhost']; ?>" />
+<h3>SAM Broadcaster Host Port</h3>
+<input type="text" name="ngs_sam_port" value="<?php echo $ngs_options['samport']; ?>" />
+<hr />
+<h2>SAM Database Settings</h2>
+<h4>These settings pertain to where your SAM database is hosted.  This is usually NOT the same as your Wordpress Database.</h4>
+<h3>SAM Database Host</h3>
+<input type="text" name="ngs_sam_db_host" value="<?php echo $ngs_options['samdbhost']; ?>" />
+<h3>SAM Database Port</h3>
+<input type="text" name="ngs_sam_db_port" value="<?php echo $ngs_options['samdbport']; ?>" />
+<h3>SAM Database Name</h3>
+<input type="text" name="ngs_sam_db_name" value="<?php echo $ngs_options['samdbname']; ?>" />
+<h3>SAM Database User</h3>
+<input type="text" name="ngs_sam_db_user" value="<?php echo $ngs_options['samdbuser']; ?>" />
+<h3>SAM Database Password</h3>
+<input type="password" name="ngs_sam_db_pwd" value="<?php echo $ngs_options['samdbpwd']; ?>" />
+<?php }
+if ('pages' == $active_tab) { ?>
+<h2>SAM Integrator Pages</h2>
+<h3>Show Queue Time</h3>
+	<label for="ngs_show_queue_time_yes">
+		<input type="radio" id="ngs_show_queue_time_yes" name="ngs_show_queue_time" 
+		   value="true" <?php if ( "true" == $ngs_options['showqueuetime'] ) echo 'checked="checked" '; ?> />
+		Yes </label>&nbsp;
+	 <label for="ngs_show_queue_time_no">
+		 <input type="radio" id="ngs_show_queue_time_no" name="ngs_show_queue_time"
+			value="false" <?php if ( "false" == $ngs_options['showqueuetime'] ) echo 'checked="checked" '; ?> />
+		 No </label>
+<h3>Default Number of Results to Show:</h3>
+<select name="ngs_default_results" id="NumPlaylistResults" >
+	<option id="List5Results" value="5" <?php if ( 5 == $ngs_options['defaultresults'] ) { echo 'selected="selected"'; } ?>>5</option>
+	<option id="List10Results" value="10" <?php if ( 10 == $ngs_options['defaultresults'] ) { echo 'selected="selected"'; } ?>>10</option>
+	<option id="List25Results" value="25" <?php if ( 25 == $ngs_options['defaultresults'] ) { echo 'selected="selected"'; } ?>>25</option>
+	<option id="List50Results" value="50" <?php if ( 50 == $ngs_options['defaultresults'] ) { echo 'selected="selected"'; } ?>>50</option>
+	<option id="List100Results" value="100" <?php if ( 100 == $ngs_options['defaultresults'] ) { echo 'selected="selected"'; } ?>>100</option>
+</select>
+<h4>If these are blank or the pages do not exist, new pages will be created and the ID's will be set 
+	automatically.<br />
+	If you manually specify page ID's, the pages will not be updated automatically, and you will need
+	to add the shortcodes into the existing pages yourself.</h4>
+<h3>Songlist By Artist Page ID</h3>
+<input type="text" name="ngs_sam_by_artist_page" value="<?php echo $ngs_options['byartistpageid']; ?>" />
+<h3>Songlist By Top Requests Page ID</h3>
+<input type="text" name="ngs_sam_by_requests_page" value="<?php echo $ngs_options['byrequestspageid']; ?>" />
+<?php }
+if ( 'status_messages' == $active_tab ) { ?>
+<h2>Custom Status Messages</h2>
+<h4>These settings will allow you to customize the messages displayed when a request can not be processed.</h4>
+<h3>Title Recently Played</h3>
+<input type="text" size="100" name="ngs_status_601" value="<?php echo $ngs_options['status601']; ?>" />
+<h3>Artist Recently Played</h3>
+<input type="text" size="100" name="ngs_status_602" value="<?php echo $ngs_options['status602']; ?>" />
+<h3>Title Already In Queue</h3>
+<input type="text" size="100" name="ngs_status_603" value="<?php echo $ngs_options['status603']; ?>" />
+<h3>Artist Already In Queue</h3>
+<input type="text" size="100" name="ngs_status_604" value="<?php echo $ngs_options['status604']; ?>" />
+<h3>Title Already Requested</h3>
+<input type="text" size="100" name="ngs_status_605" value="<?php echo $ngs_options['status605']; ?>" />
+<h3>Artist Already Requested</h3>
+<input type="text" size="100" name="ngs_status_606" value="<?php echo $ngs_options['status606']; ?>" />
+<h3>Track Recently Played</h3>
+<input type="text" size="100" name="ngs_status_609" value="<?php echo $ngs_options['status609']; ?>" />
+<?php } ?>
+<div class="submit">
+<input type="submit" name="update_ngs_sam_integrator_settings" value="<?php _e('Update Settings', 'NGS_SAM_Integrator') ?>" /></div>
+</form>
+
+<?php // Paypal Donation Code ?>
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top"><input type="hidden" name="cmd" value="_s-xclick" />
+<input type="hidden" name="hosted_button_id" value="9T8FRUACJJM9Y" />
+<input type="image" alt="PayPal - The safer, easier way to pay online!" name="submit" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" />
+<img alt="" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1" border="0" /></form>
+    
+ </div>
---
+++ php/request.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * Request handling code
+ * 
+ * All communication between the website and the SAM Broadcaster client 
+ * software takes place here.
+ * 
+ * @package NGS SAM Integrator
+ * @since 0.1.0
+ */
+
+/*
+ * if the 'songid' parameter is found in the query string of the URL, we will
+ * attempt to process it as a request.
+ */
+if ( isset( $_GET['songid'] ) ) {
+    $song_id = absint( $_GET['songid'] ); 
+    $requested_song = $samdb->get_row( $samdb->prepare( "SELECT artist, title FROM songlist WHERE id=%d", $song_id ) );
+	$samerr = 0;
+	
+    /*
+     *  Do some preliminary data validation and pass appropriate error codes if
+     *  the data doesn't exist.
+     */
+	
+	if ( empty( $samhost ) )
+		$samerr =  800;
+/*    if ( ( "127.0.0.1" == $samhost ) OR ( "localhost" == $samhost ) )
+    	$samerr = 801; */
+    if ( empty( $song_id ) )
+		$samerr = 802;
+
+    $client_host = $_SERVER["REMOTE_ADDR"]; 
+    $request = "GET /req/?songID=$song_id&host=".urlencode($client_host)." HTTP\1.0\r\n\r\n";
+    $request_response_xml = '';
+    $request_file_handle = @fsockopen( $samhost,$samport, $errno, $errstr, 30 );
+	
+    if ( ! empty( $request_file_handle ) ) {
+    	fwrite( $request_file_handle, $request );
+	while ( ! ( "\r\n" == $line ) )
+            $line = fgets( $request_file_handle, 128 ); 
+	while ( $request_buffer = fgets( $request_file_handle, 4096 ) )
+            $request_response_xml .= $request_buffer;
+            fclose( $request_file_handle );
+    } else { 
+		$samerr = 803;
+    }
+	
+    if ( empty( $request_response_xml ) )
+		$samerr = 804;
+	
+    $request_xml_array = XMLtoArray($request_response_xml);
+    $request_result = Keys2Lower($request_xml_array['REQUEST']);
+    $request_status_code = $request_result['status']['code'];
+    $request_status_message = $request_result['status']['message'];
+    $request_status_id = $request_result['status']['requestid'];
+	
+    if ( empty( $request_status_code ) )
+		$samerr = 804;
+	
+	if ( 0 != $samerr )
+		ngs_sam_error( $samerr );
+    else if ( 200 == $request_status_code  )
+		require_once dirname( __FILE__ ).'/request-success.php';
+ 	else
+		ngs_status_code( $request_status_code, $requested_song, $request_status_message, $ngs_options );
+
+}
+/**
+ * 
+ * @param integer $code
+ * @param string $samhost
+ * @param integer $samport
+ * @param integer $errno
+ * @param string $errstr
+ */
+function ngs_sam_error( $code )
+{
+    global $samhost, $samport, $errno, $errstr, $requested_song;
+    switch( $code )
+    {
+	case 800 : $request_status_message = "SAM host must be specified"; break;
+	case 801 : $request_status_message = "SAM host can not be 127.0.0.1 or localhost"; break;
+	case 802 : $request_status_message = "Song ID must be valid";  break;
+	case 803 : $request_status_message = "Unable to connect to $samhost:$samport. Station might be offline.<br>The error returned was $errstr ($errno).";  break;
+	case 804 : $request_status_message = "Invalid data returned!";  break;
+	default : $request_status_message = "Unknown SAM Error Code ($code) Returned"; break;
+    }
+    require dirname( __FILE__ ).'/request-failure.php';
+}
+
+function ngs_status_code( $code, $requested_song, $request_status_message, $ngs_options )
+{
+	switch( $code )
+	{
+		case 601 : $request_status_message = $ngs_options['status601']; break;
+		case 602 : $request_status_message = $ngs_options['status602']; break;
+		case 603 : $request_status_message = $ngs_options['status603']; break;
+		case 604 : $request_status_message = $ngs_options['status604']; break;
+		case 605 : $request_status_message = $ngs_options['status605']; break;
+		case 606 : $request_status_message = $ngs_options['status606']; break;
+		case 609 : $request_status_message = $ngs_options['status609']; break;
+		default : break;
+	}
+	require dirname(__FILE__).'/request-failure.php';
+}
